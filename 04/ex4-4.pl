85d92364df3beac32bed2f40e20fb55f79a40e36
okitan <okitakunio@gmail.com>
2012-03-26 17:11:05 +0900

    I forgot ex4-5

--- 04/ex4-4.pl
+++ 04/ex4-4.pl
@@ -5,11 +5,14 @@ use strict;
 use warnings;
 
 sub greet {
-  state $last_one;
-  (my $before, $last_one) = ($last_one, @_);
+  state @last_ones;
 
-  say "Hello @_, " . ( $before ? "I met $before, before" : "" );
+  say "Hi @_! " . ( @last_ones ? "I have seen: @last_ones" : "You are the first one here!" );
+
+  push(@last_ones, @_);
 }
 
 greet "Fred";
 greet "Barney";
+greet "Wilma";
+greet "Betty";
